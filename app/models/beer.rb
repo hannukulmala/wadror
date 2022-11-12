b2354f162b7bb2f0f59d158e2ae36c9d4a7b0e75
Hannu Kulmala <hanski22@pm.me>
2022-11-12 18:49:07 +0200

    Viikko2 Tehtava4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,20 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+       sum = self.ratings.reduce (0) {|sum,element| sum + element.score}
+       count = self.ratings.count
+       (sum / count).to_f
+    end
+
+    def print_rating
+      num_ratings = self.ratings.count
+      if num_ratings == 1
+        "Beer has #{self.ratings.count} rating with a score of #{self.average_rating}"
+      else 
+        "Beer has #{self.ratings.count} ratings with an average of #{self.average_rating}"
+      end
+    end
+     
 end
