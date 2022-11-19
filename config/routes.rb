962b05009bf3cc85c28dbd3e9b9104c7c9ef7e95
Hannu Kulmala <hanski22@pm.me>
2022-11-19 20:29:06 +0200

    Viikko3 Tehtava3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   root 'breweries#index'
+  get 'signup', to: 'users#new'
 end
