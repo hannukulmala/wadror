d6e2071062d793cdfeca4e9093bc7e3c10c97f20
Hannu Kulmala <hanski22@pm.me>
2022-11-21 16:27:42 +0200

    Viikko3 Tehtava12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   include RatingAverage
+  has_secure_password
+  PASSWORD_VALIDATION_REGEX = /\A(?=.*[A-Z])(?=.*\d)(?=.{4,})/x
+
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
+  validates :password, format: PASSWORD_VALIDATION_REGEX
   has_many :ratings
   has_many :beers, through: :ratings
   has_many :memberships
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_132630) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_132527) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_132630) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
