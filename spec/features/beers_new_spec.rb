a4a0090e1abc926316a2d87b8372a914583cac6f
Hannu Kulmala <hanski22@pm.me>
2022-11-27 11:49:16 +0200

    Viikko4 Tehtava5

---
+++ spec/features/beers_new_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+include Helpers
+
+describe "New beer" do
+  let!(:brewery) { FactoryBot.create :brewery, name: "Koff" }
+  let!(:user) { FactoryBot.create :user }
+
+  before :each do
+    sign_in(username: "Pekka", password: "Foobar1")
+  end
+
+  it "is added when given non-empty name" do
+    visit new_beer_path
+    fill_in('beer[name]', with: 'a')
+    select('IPA', from: 'beer[style]')
+    select(brewery.name, from: 'beer[brewery_id]')
+
+    expect{
+      click_button "Create Beer"
+    }.to change{Beer.count}.from(0).to(1)
+  end
+
+  it "is not added with an empty name and error message is shown" do
+    visit new_beer_path
+    fill_in('beer[name]', with: '')
+    select('IPA', from: 'beer[style]')
+    select(brewery.name, from: 'beer[brewery_id]')
+
+    expect{
+      click_button "Create Beer"
+    }.not_to change{Beer.count}
+    expect(Beer.count).to eq(0)
+    expect(page).to have_content "Name can't be blank"
+
+  end
+  
+
+end
